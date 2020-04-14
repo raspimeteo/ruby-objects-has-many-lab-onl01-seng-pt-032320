2f5db2225c1b8cc04353745990e2e5d2e26c76bc
John Zonneveld <jjczon@gmail.com>
2020-04-14 17:07:17 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,39 +1,33 @@
 require 'pry'
 class Author
-    ## add attribute getters and setters
     attr_accessor :name
 
     def initialize(name)
-        ## Initialize a new artist wit its name
         @name = name
-        ## create an empty list for its songs
         @posts = []
     end
 
-    def add_post(post)
-        ## add song to the @songs list
-        @posts << post
-        ## make a song belong to its artist
-        post.author = self
-    end
-
     def posts
-        ## return all songs in the @songs list
+        ## return all posts in the @posts list
         Post.all.select {|post| post.author == self}
     end
 
-    def add_post_by_title (title)
-      ##binding.pry
-      post = Post.new(title)
-      add_post(post)
-      ##binding.pry
-      
-      
+    def add_post(post)
+        ## add post to the @post list
+        @posts << post
+        ## make a post belong to its artist
+        post.author = self
+    end
 
-    end
-    
-    def self.post_count
-      Post.all.count
-    end
+    def add_post_by_title(name)
+        post = Post.new(name)
+        ##binding.pry
+        add_post(post)
+        ##binding.pry
+      end
+
+      def self.post_count
+        Post.all.count
+      end
 
 end
